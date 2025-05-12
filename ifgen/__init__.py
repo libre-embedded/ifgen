1abaa57b7b84b8570738bc301f3799ad38c33f3b
Vaughn Kottler <vaughn@libre-embedded.com>
2025-05-12 04:38:25 -0500

    4.4.4 - Use uppercase namespace

--- ifgen/__init__.py
+++ ifgen/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.3
-# hash=520db3f30066c96a8613f5361eca3772
+# hash=df27db53be175c0270252c83f57d166d
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "An interface generator for distributed computing."
 PKG_NAME = "ifgen"
-VERSION = "4.4.3"
+VERSION = "4.4.4"
