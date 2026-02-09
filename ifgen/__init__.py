f4c009fbced785ce4acd7bcac1872e9f69852a53
Vaughn Kottler <vaughn@libre-embedded.com>
2026-02-09 02:51:04 +0000

    4.7.1 - mmio structs still need regular arrays
    due to volatile discard on array methods, not an issue with
    C arrays due to indexing functioning via pointer decomposition?

--- ifgen/__init__.py
+++ ifgen/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.3
-# hash=18b776569e15d4363f48bde945c2b0f6
+# hash=415fefcf7736b4ad3c659e0cd455c32e
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "An interface generator for distributed computing."
 PKG_NAME = "ifgen"
-VERSION = "4.7.0"
+VERSION = "4.7.1"
